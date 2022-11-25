7347114c864b3aadb126502895b9332f38e8f59b
OlhaNekrylova <nekrylovaurist@gmail.com>
2022-11-25 20:28:33 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+import '../css/common.css';
+
+const ref = {
+    startBtn : document.querySelector('button[data-start]'),
+    stopBtn : document.querySelector('button[data-stop]'),
+};
+
+let intervalId = null;
+const isActive = false;
+
+ref.startBtn.addEventListener('click', ()  => {
+    const intervalId = setInterval(() => {
+        getRandomHexColor();
+    }, 1000);
+    
+});
+
+ref.stopBtn.addEventListener('click', () => {
+    clearInterval(intervalId); 
+});
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
