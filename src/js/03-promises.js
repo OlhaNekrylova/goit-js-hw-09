c4d9cbb91db07bfac50071457acda975cc9d57f5
OlhaNekrylova <nekrylovaurist@gmail.com>
2022-11-29 00:58:45 +0200

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,56 +1,64 @@
 import '../css/common.css';
 
-refs = {
+const refs = {
   form : document.querySelector('.form'),
-  inputDdelay : document.querySelector('input[name="delay"]'),
+  inputDelay : document.querySelector('input[name="delay"]'),
   inputStep : document.querySelector('input[name="step"]'), 
   inputAmount : document.querySelector('input[name="amount"]'),
 };
 
-refs.form.addEventlistener('submit', onFormSubmit);
-// refs.inputDdelay.addEventlistener('submit', onFormSubmit);
-// refs.inputStep.addEventlistener('submit', onFormSubmit);
-// refs.inputAmount.addEventlistener('submit', onFormSubmit);
+refs.form.addEventListener('submit', onFormSubmit);
+refs.inputDelay.addEventListener('submit', onFormSubmit);
+refs.inputStep.addEventListener('submit', onFormSubmit);
+refs.inputAmount.addEventListener('submit', onFormSubmit);
 
-const firstDelay = refs.inputDelay.value;
-const delayStep = refs.inputStep.value;
-const delay = firstDelay + delayStep;
+const delay = refs.inputDelay.value;
+const step = refs.inputStep.value;
 const AMOUNT = refs.inputAmount.value;
 let position = 0;
 let counter = 0;
-let intervalId = null;
+let timeoutId = null;
 
 function createPromise(position, delay) {
   
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
-    counter = AMOUNT;
-
-    const intervalId = setInterval(() => {
+    
+    const timeoutId = setTimeout(() => {
       // if (counter === AMOUNT) {
       //   clearInterval(intervalId);
       //   return;
       // }
 
       if (shouldResolve) {
-        resolve();// Fulfill
+        resolve({ position, delay });
       } else {
-        reject();// Reject
+        reject({ position, delay });
       }
-      position += 1;
-      counter += 1;
+      
     }, delay);
   });
 }
 
 function onFormSubmit(evt) {
-  createPromise(position, delay)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
+  evt.preventDefault();
+  
+  counter = AMOUNT;
+  // if (counter === AMOUNT) {
+
+  // }
+
+    createPromise(position, delay)
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+
+    position += 1;
+    delay += step;
+  
 }
 
 
