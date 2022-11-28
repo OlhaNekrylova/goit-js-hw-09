59cfe7700dd0dde9e2388a2d70c56fe47e3ffaf1
OlhaNekrylova <108542731+OlhaNekrylova@users.noreply.github.com>
2022-11-28 23:47:28 +0000

    Deploying to gh-pages from @ OlhaNekrylova/goit-js-hw-09@cb2217fd372048079f133cfd8ed851cb28093dac 🚀

--- 01-color-switcher.448ec597.js
+++
@@ -1,2 +0,0 @@
-const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};let n=null;t.startBtn.addEventListener("click",(function(t){n=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.stopBtn.addEventListener("click",(function(t){clearInterval(n)}));
-//# sourceMappingURL=01-color-switcher.448ec597.js.map
